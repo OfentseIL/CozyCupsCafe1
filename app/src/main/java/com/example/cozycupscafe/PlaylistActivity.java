44663ae85d142a189db55cdce2c37cdac8d5767e
Study-Hall <ofentseinnocentlehutjo@gmail.com>
2024-09-27 22:25:06 +0200

    fridayy

--- app/src/main/java/com/example/cozycupscafe/PlaylistActivity.java
+++ app/src/main/java/com/example/cozycupscafe/PlaylistActivity.java
@@ -1,6 +1,9 @@
 package com.example.cozycupscafe;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PlaylistActivity extends AppCompatActivity {
 
+    private Button playButton1;
+    private Button playButton2;
+    private Button playButton3;
+    private MediaPlayer mediaPlayer;
+    private final boolean isPlaying = false; // Track if a song is playing
+    private final int currentSong = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        playButton1 = findViewById(R.id.playButton1);
+        playButton2 = findViewById(R.id.playButton2);
+        playButton3 = findViewById(R.id.playButton3);
+        Button menuButton = findViewById(R.id.menuButton);
+
+        playButton1.setOnClickListener(v -> playSong(R.raw.morning, playButton1));
+        playButton2.setOnClickListener(v -> playSong(R.raw.afternoon, playButton2));
+        playButton3.setOnClickListener(v -> playSong(R.raw.relaxing, playButton3));
+
+        menuButton.setOnClickListener(v -> {
+            Intent intent = new Intent(PlaylistActivity.this, GridActivity.class);
+        });
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_playlist);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +43,11 @@ public class PlaylistActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void playSong(int songResId, Button playButton){
+        {
+
+        }
     }
 }
